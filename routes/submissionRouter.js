1493a5112ccc694ad1efae842bb1e836c60e191a
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2017-05-30 23:10:58 -0500

    Minor fix

--- routes/submissionRouter.js
+++ routes/submissionRouter.js
@@ -250,7 +250,7 @@ router.get('/user/:userName',function(req,res,next){
 });
 router.route('/idUser/:userId')
 .delete(Verify.verifyAdminUser,function(req,res,next){
-  Submissions.remove({'userId':req.params.userId},unction (err, resp) {
+  Submissions.remove({'userId':req.params.userId},function (err, resp) {
     if (err) throw err;
     res.json(resp);
   });
@@ -289,7 +289,7 @@ router.route('/problem/:problemName')
 });
 router.route('/idProblem/:problemId')
 .delete(Verify.verifyAdminUser,function(req,res,next){
-  Submissions.remove({'problemId':req.params.problemId},unction (err, resp) {
+  Submissions.remove({'problemId':req.params.problemId},function (err, resp) {
     if (err) throw err;
     res.json(resp);
   });
